56ff356260c84bab4c1ad9d0522fe87100aa6b68
Praveen <96812498+162010121@users.noreply.github.com>
2023-12-29 12:06:11 +0530

    Delete shoppingcartCrudSpringBoot-main directory
    delete

--- shoppingcartCrudSpringBoot-main/src/main/java/com/ashish/shoppingcart/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ashish.shoppingcart.repository;
-
-import com.ashish.shoppingcart.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
